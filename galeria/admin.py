3ac4e3c67e9baac28f05ba4fee4b8bafddb0ebf6
L F <franco100lucas@gmail.com>
2024-02-22 05:39:28 -0300

    Criando novo campo no banco e personalizando django admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda') # Campos que serão exibidos na listagem
+    list_display_links = ('id', 'nome') # Campos que serão links para a edição
+    search_fields = ('nome',) # Campos que serão pesquisáveis
+
+admin.site.register(Fotografia, ListandoFotografias)  # Campo para registrar o modelo no admin
