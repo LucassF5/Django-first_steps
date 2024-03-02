4a5be83afc6fe8f6265616859aed4a380c333dd9
L F <franco100lucas@gmail.com>
2024-03-02 09:33:32 -0300

    Refatorando código e adicionando autenticação e redirecionamento

--- galeria/views.py
+++ galeria/views.py
@@ -1,21 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from galeria.models import Fotografia
+from django.contrib import messages
+
 
 def index(request):
-    fotografias = Fotografia.objects.order_by('-id').filter(publicada=True) 
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect("login")
+    
+    fotografias = Fotografia.objects.order_by('-id').filter(publicada=True)
     # Vai buscar todas as fotos que estão publicadas
     # O método order_by('-id') vai buscar as fotos na ordem decrescente de id
-    
+
     return render(request, "galeria/index.html", {"cards": fotografias})
 
+
 def imagem(request, foto_id):
-    fotografia = get_object_or_404(Fotografia, pk=foto_id) #pk = primary key
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)  # pk = primary key
     # Vai receber o id da foto e buscar a foto no banco de dados
     # Caso não encontre, retorna um erro 404
     return render(request, "galeria/imagem.html", {"fotografia": fotografia})
 
+
 def buscar(request):
-    fotografias = Fotografia.objects.order_by('-id').filter(publicada=True) 
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect("login")
+    fotografias = Fotografia.objects.order_by('-id').filter(publicada=True)
 
     if "buscar" in request.GET:
         nome_a_buscar = request.GET["buscar"]
