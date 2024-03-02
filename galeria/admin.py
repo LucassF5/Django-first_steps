8b3ff1e6a82681edb28794f526d87962dfd92895
L F <franco100lucas@gmail.com>
2024-03-02 14:15:20 -0300

    Inserindo inserção por usuário

--- galeria/admin.py
+++ galeria/admin.py
@@ -7,7 +7,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ('id', 'nome', 'legenda', 'publicada') # Campos que serão exibidos na listagem
     list_display_links = ('id', 'nome') # Campos que serão links para a edição
     search_fields = ('nome',) # Campos que serão pesquisáveis
-    list_filter = ["categoria"] # Campos que terão filtro
+    list_filter = ["categoria", "usuario"] # Campos que terão filtro
     list_editable = ['publicada'] # Campos que podem ser editados diretamente na listagem
     list_per_page = 10 # Quantidade de itens por página
     
